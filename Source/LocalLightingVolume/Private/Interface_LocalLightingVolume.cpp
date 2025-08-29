1cc1b7d16daf678dca3dc7e32e9e45bea6f8fcb4
Cplus <1248114644@qq.com>
2025-08-30 00:38:21 +0800

    Technical Artist - Jiahao.Chan
    comment

--- Source/LocalLightingVolume/Private/Interface_LocalLightingVolume.cpp
+++ Source/LocalLightingVolume/Private/Interface_LocalLightingVolume.cpp
@@ -1,7 +1,7 @@
 /**
  * Plugin LocalLightingVolume
  *		Allow to modify global Light Component such as Sky Light & Directional Light when View Point in the range of Volume.
- * Copyright TA.Jiahao.Chan, Inc. All Rights Reserved.
+ * Copyright Technical Artist - Jiahao.Chan, Individual. All Rights Reserved.
  */
 
 #include "Interface_LocalLightingVolume.h"
--- Source/LocalLightingVolume/Private/LocalLightingVolume.cpp
+++ Source/LocalLightingVolume/Private/LocalLightingVolume.cpp
@@ -1,7 +1,7 @@
 /**
  * Plugin LocalLightingVolume
- *		Allow to modify global Light Component such as Sky Light & Directional Light when View Point in the range of Volume.
- * Copyright TA.Jiahao.Chan, Inc. All Rights Reserved.
+ *      Allow to modify global Light Component such as Sky Light & Directional Light when View Point in the range of Volume.
+ * Copyright Technical Artist - Jiahao.Chan, Individual. All Rights Reserved.
  */
 
 #include "LocalLightingVolume.h"
